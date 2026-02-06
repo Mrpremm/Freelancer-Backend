a8be8a92612327cda08b55cbaca49dc9f00b7e8a
Prem Prasad <premrinkinoniya@gmail.com>
2026-02-06 16:22:06 +0530

    Updating Code

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const connectDB=require("./server/config/db");
 require("dotenv").config();
 
 
+const PORT =process.env.PORT||3000;
+
+
 connectDB();
 
+const server=app.listen(PORT,()=>{
+  console.log(`Server is running  on port ${PORT}`);
+  
+});
+process.on("unhandledRejection",(err,promise)=>{
+  console.error(`Error: ${err.message}`);
+  server.close(()=>{process.exit(1)});
+})
 

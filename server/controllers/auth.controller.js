d225567fc355a31258b7627052829f0ff37920a0
Prem Prasad <premrinkinoniya@gmail.com>
2026-02-06 22:39:01 +0530

    Updating Code

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -14,5 +14,25 @@ const registerUser =asyncHandler(async (req,res)=>{
     throw new Error('User already exists');
   }
 
+//Create User
+const user=await User.create({
+  name,
+  email,
+  password,
+  role:role|| 'client',
+  
+});
+if(user){
+  res.status(201).json({
+    _id:user._id,
+    name:user.name,
+    email:user.email,
+    role:user.role,
+    token:generateToken(user._id),
+  });
 
+}else{
+  res.status(400);
+  throw new Error('Invalid user data');
+}
 })

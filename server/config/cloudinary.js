05234bb997678a83b90c5a8ace4b736d5db15adf
Prem Prasad <premrinkinoniya@gmail.com>
2026-02-06 16:39:38 +0530

    Updating Code

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,3 +1,24 @@
 const cloudinary=require("cloudinary").v2
 const {CloudinaryStorage}=require("multer-storage-cloudinary");
 
+cloudinary.config({
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET,
+});
+
+//Storage ENgine for Multer
+
+const storage=new CloudinaryStorage({
+  cloudinary:cloudinary,
+  params:{
+    folder:"freelance-marketplace",
+    allowed_formats:['jpg','png','jpeg','gif','webp','svg'],
+    transformation:[{
+      width:500,
+      height:500,
+      crop:"fill"
+    }],
+  },
+});
+module.exports={cloudinary,storage}
